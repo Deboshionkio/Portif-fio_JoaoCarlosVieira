e2f35cf184f54a0294c3d8b7b63a2e517462669f
João Carlos Vieira <89552338+Deboshionkio@users.noreply.github.com>
2022-07-31 17:28:50 -0300

    adicionados index e etc.

---
+++ trocaImagem.js
@@ -0,0 +1,22 @@
+function mostraBeijaflor(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/beijaflor.jpeg";
+   }
+function mostraSecador(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/secador.jpg";
+   }
+
+
+function inverteEstudante(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/estudante.jpeg";
+    }    
+function inverteEstudo(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/estudo.jpg";
+    }
+
+
+function trocaSax(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/sax.jpeg";
+    }
+function trocaSoprador(imagem){
+    imagem.src="file:///C:/Users/55489/OneDrive%20-%20UNIVALI/Documentos/visualcode%20projetos%20portifolio/imagens/soprador.jpg";
+    }
